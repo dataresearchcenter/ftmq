f032a0ada8710d6efd3e0d92e684040e4128dc41
Simon Wörpel <simon.woerpel@pm.me>
2025-07-02 13:55:14 +0200

    🚚 (model) Entity -> EntityModel

--- ftmq/model/__init__.py
+++ ftmq/model/__init__.py
@@ -1,12 +1,12 @@
 from ftmq.model.coverage import DatasetStats
 from ftmq.model.dataset import Catalog, Dataset, Maintainer, Publisher, Resource
-from ftmq.model.proxy import Entity
+from ftmq.model.entity import EntityModel
 
 __all__ = [
     "Catalog",
     "DatasetStats",
     "Dataset",
-    "Entity",
+    "EntityModel",
     "Maintainer",
     "Publisher",
     "Resource",
