0aab85c629d0ca6043721d0f0d539a7f88a99672
Simon Wörpel <simon.woerpel@pm.me>
2025-07-28 17:01:24 +0200

    🔖 Bump version: 4.0.2 → 4.1.0

--- ftmq/__init__.py
+++ ftmq/__init__.py
@@ -2,7 +2,7 @@ from ftmq.io import smart_read_proxies, smart_write_proxies
 from ftmq.query import Query
 from ftmq.util import make_entity
 
-__version__ = "4.0.2"
+__version__ = "4.1.0"
 __all__ = [
     "smart_read_proxies",
     "smart_write_proxies",
