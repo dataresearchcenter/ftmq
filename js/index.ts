1378e1be599cc05580b2ff299cc126c249ae5133
Simon Wörpel <simon.woerpel@pm.me>
2026-04-07 15:01:51 +0200

    🔥 Drop js lib

--- js/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./api";
-export * from "./model";
-export * from "./util"
-export { default as Api } from "./api";
--- js/model/index.ts
+++
@@ -1,9 +0,0 @@
-export * from "./entity";
-export * from "./model";
-export * from "./property";
-export * from "./schema";
-export * from "./propertyType";
-export * from "./util";
-
-// dataset model types
-export * from "./dataset";
